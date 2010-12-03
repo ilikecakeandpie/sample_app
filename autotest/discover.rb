cc184557dc122481263424b22bda3a40e054af82
ilikecakeandpie <john.harris@complymd.com>
2010-12-03 12:23:36 -0600

    Removed default rails page

--- autotest/discover.rb
+++ autotest/discover.rb
@@ -1,2 +1,7 @@
 Autotest.add_discovery { "rails" }
 Autotest.add_discovery { "rspec2" }
+Autotest.add_hook :initialize do |autotest|
+  autotest.add_mapping(%r%^spec/(requests)/.*rb$%) do|filename, _|
+    filename
+  end
+end 
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,12 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  before(:each) do
+    #
+    # define @base_title here.
+    #
+  end
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -27,6 +33,17 @@ describe PagesController do
   end
 
   describe "GET 'about'" do
+      it "should be successful" do
+        get 'about'
+        response.should be_success
+    end
+   it "should have the right title" do
+      get 'home'
+      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Home")
+    end
+  end
+
+  describe "GET 'help'" do
       it "should be successful" do
         get 'about'
         response.should be_success
